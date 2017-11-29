eb94488ff4136ad3aedcc9b10f8c176acf1378c8
Pierluigi D'Andrea <pierluigi.dandrea@satispay.com>
2017-11-29 09:28:37 +0100

    Update the OpenSSL build script

--- OpenSSL-Scripts/build.sh
+++ OpenSSL-Scripts/build.sh
@@ -5,8 +5,8 @@ if [ -f lib/libssl.a ] && [ -f lib/libcrypto.a ] && [ -d "$1" ]; then
 fi
 
 OPTIONS="no-ssl2 no-ssl3 no-comp no-async no-psk no-srp no-dtls no-dtls1"
-OPTIONS+=" no-ec no-ec2m no-engine no-hw no-filenames no-err"
-OPTIONS+=" no-bf no-blake2 no-camellia no-cast no-chacha no-cmac no-des no-ecdh no-ecdsa no-idea no-md4 no-mdc2 no-ocb no-poly1305 no-rc2 no-rc4 no-rmd160 no-scrpyt no-seed no-siphash no-sm3 no-sm4 no-whirlpool"
+OPTIONS+=" no-ec no-ec2m no-engine no-hw no-err"
+OPTIONS+=" no-bf no-blake2 no-camellia no-cast no-chacha no-cmac no-ecdh no-ecdsa no-idea no-md4 no-mdc2 no-ocb no-poly1305 no-rc2 no-rc4 no-rmd160 no-scrpyt no-seed no-siphash no-sm3 no-sm4 no-whirlpool"
 OPTIONS+=" -Wno-error=ignored-optimization-argument"
 
 export CONFIG_OPTIONS="$OPTIONS"
